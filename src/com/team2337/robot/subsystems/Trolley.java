13bafc196ee96be7c452b8dc3a2b4704571e97d8
slynch05 <slynch05@yahoo.com>
2018-02-17 19:51:50 -0500

    New Trolley Points

--- src/com/team2337/robot/subsystems/Trolley.java
+++ src/com/team2337/robot/subsystems/Trolley.java
@@ -23,10 +23,10 @@ public class Trolley extends Subsystem {
 
 	//private int absolutePosition;  //used to set relative position encoder
 	private double maxSpeedUp = 1.0;
-	private double maxSpeedDown = 0.5; //0.01
+	private double maxSpeedDown = 1.0; //0.01
 	private double nominalSpeed = 0;
 	private double kF = 0;
-	private double kP = 7;
+	private double kP = 14;
 	private double kI = 0;
 	private double kD = 0;
 	private int allowableError = 0;
