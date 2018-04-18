56504205f5d62d8c272195265baa1d9516b91951
slynch05 <slynch05@yahoo.com>
2018-04-17 21:27:57 -0400

    Grab cube on way to scale from switch

--- src/com/team2337/robot/commands/auto/CG_autoFavorOurScaleSoloFromPyramid.java
+++ src/com/team2337/robot/commands/auto/CG_autoFavorOurScaleSoloFromPyramid.java
@@ -34,31 +34,32 @@ public class CG_autoFavorOurScaleSoloFromPyramid extends CommandGroup {
 			addParallel(new auto_bigBrother_DoNothing());
 			addSequential(new auto_holdUpperPosition(0.1));
 			//STOP  DO NOT CHANGE THE ABOVE OR PUT ANY CODE BEFORE THESE LINES YOU WILL SEND THE TROLLEY FLYING!!!!!
-			addSequential(new commonCG_scoreLeftSwitch());
+//				addSequential(new commonCG_scoreLeftSwitch());
 			addParallel(new commonCG_armToPickupFromFront());
 //			addSequential(new commonCG_driveAroundSwitchRight());
 			addSequential(new auto_wait(.25));
 			
 			
 			addSequential(new auto_resetEncoder());
-			addSequential(new auto_driveToAngleWithEncoder(-.5, 3, 0, 22000, 22000, 0.04));
+			addSequential(new auto_driveToAngleWithEncoder(-.5, 3, 0, 24000, 24000, 0.04));
 			addSequential(new auto_clawOpen());
 			
-			addSequential(new auto_gyroMMTurn(-75, 0.75));
-			
-			addParallel(new auto_driveToAngleWithEncoder(.7, 8, -75, 75000, 75000, 0.025, true));
-			addSequential(new auto_wait(1.5));
-			addParallel(new auto_intake_in(.7, 1));
-			addSequential(new auto_wait(5));
-			addSequential(new auto_clawClose());
-			addSequential(new auto_claw60());
-			addSequential(new auto_driveToAngleWithEncoder(.7, 8, -75, 101000, 101000, 0.025, true));
+			addSequential(new auto_gyroMMTurn(105, 0.75));
+			addSequential(new auto_moveUpperPosition(3300,45)); // 80 practice);
+			addSequential(new auto_resetEncoder());
 			
 			
-			/*
+			addSequential(new auto_driveToAngleWithEncoder(-.7, 8, 105, 20000, 20000, 0.025, "Left"));
+			addSequential(new auto_driveToAngleWithEncoder(-.7, 8, 85, 85000, 85000, 0.04, "Left"));	//44000
+//				addSequential(new auto_wait(0.7));
+				addSequential(new auto_clawClose());
+				addSequential(new auto_claw60());
 			
-			addSequential(new auto_driveToAngleWithEncoder(.6, 8, 0, 210000, 210000, 0.01));
+			addSequential(new auto_driveToAngleWithEncoder(-.6, 8, 180, 130000, 130000, 0.03, "Right"));
+			addSequential(new auto_moveUpperPosition(2700,45));
+			addSequential(new auto_driveToAngleWithEncoder(-.6, 8, 180, 200000, 200000, 0.03, "Right"));
 			addSequential(new auto_brakeModeOn());
+			/*
 			addSequential(new auto_clawCGOpenClose());
 			addParallel(new auto_moveUpperPosition(3300,45)); // 80 practice
 			addSequential(new auto_resetEncoder());
